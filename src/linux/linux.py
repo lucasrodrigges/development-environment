fb87ac7f5f698419388c16ef755b9ccc4627418b
lucas <lucas@pop-os.localdomain>
2023-05-10 09:09:34 -0300

    fix zsh scrip patht

--- src/linux/linux.py
+++ src/linux/linux.py
@@ -15,7 +15,7 @@ def customize_terminal():
     print(right_arrow + "Customizing terminal" + left_arrow)
 
     subprocess.run(["sudo", "apt", "install", "zsh", "-y"])
-    subprocess.run(["curl", "-fsSL", "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"])
+    subprocess.run(["sh", "-c", "'$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'"])
     subprocess.run(["sudo", "chsh", "-s", "$(which zsh)"])
     subprocess.run(["git", "clone", "https://github.com/zsh-users/zsh-syntax-highlighting.git", "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"])
     subprocess.run(["git", "clone", "https://github.com/zsh-users/zsh-autosuggestions", "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"])
