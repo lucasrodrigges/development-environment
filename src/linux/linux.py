345a09b24e1db9db4890e14638af899d154e8510
lucas <lucas@pop-os.localdomain>
2023-05-10 19:16:37 -0300

    remove custom terminal functions

--- src/linux/linux.py
+++ src/linux/linux.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
-from src.utils.arrows import left_arrow, right_arrow
 from src.linux.utils.dev_softwares import install_dev_softwares
+
+from src.utils.arrows import left_arrow, right_arrow
 
 current_dir = os.getcwd()
 
@@ -13,40 +14,6 @@ def system_update():
     subprocess.run(["sudo", "apt", "upgrade", "-y"])
 
 
-def customize_terminal():
-    print(right_arrow + "Customizing terminal" + left_arrow)
-
-    subprocess.run(["sudo", "apt", "install", "zsh", "-y"])
-    subprocess.run([
-        "wget",
-        "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh",
-        "&&",
-        "sh",
-        "install.sh"
-    ])
-    subprocess.run(["sudo", "chsh", "-s", "$(which zsh)"])
-    subprocess.run([
-        "git",
-        "clone",
-        "https://github.com/zsh-users/zsh-syntax-highlighting.git",
-        "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-    ])
-    subprocess.run([
-        "git",
-        "clone",
-        "https://github.com/zsh-users/zsh-autosuggestions",
-        "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-    ])
-
-    with open("~/.zshrc", "a") as file:
-        file.write('export ZSH="$HOME/.oh-my-zsh"\n')
-        file.write('ZSH_THEME="robbyrussell"\n')
-        file.write(
-            'plugins=(git zsh-syntax-highlighting zsh-autosuggestions)\n'
-        )
-
-
 def linux_script():
     system_update()
-    customize_terminal()
     install_dev_softwares()
