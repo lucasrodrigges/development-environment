01673a02081cdbeafdf82c81b101d73fbf100ec2
lucas <lucas@pop-os.localdomain>
2023-05-10 01:46:43 -0300

    fix zsh scrip patht

--- src/linux/linux.py
+++ src/linux/linux.py
@@ -1,3 +1,4 @@
+import os
 from src.utils.arrows import left_arrow, right_arrow
 import subprocess
 from src.linux.utils.dev_softwares import install_dev_softwares
@@ -10,8 +11,8 @@ def system_update():
 def customize_terminal():
     print(right_arrow + "Customizing terminal" + left_arrow)
     subprocess.run(["sudo", "apt", "install", "zsh", "-y"])
-    subprocess.run(["./scripts/oh_my_zsh_install.sh"])
-    subprocess.run(["./scripts/zsh_customization.sh"])
+    subprocess.run(["home/$USER/Downloads/development-environment/src/linux/scripts/oh_my_zsh_install.sh"])
+    subprocess.run(["home/$USER/Downloads/development-environment/src/linux/scripts/zsh_customization.sh"])
 
 def linux_script():
     system_update()
