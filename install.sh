2ff782be87ed5940d1ca9505e1a2f247b40f75bc
lucas <lucas@pop-os.localdomain>
2023-05-10 01:28:40 -0300

    add shell script header

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-if ! which python3 > /dev/null; then
-    echo "Installing Python 3..."
-    sudo apt-get install python3 python3-pip python3.10-venv -y 
-fi
+#!/bin/bash
+
+echo "Installing Python 3..."
+sudo apt-get install python3 python3-pip python3.10-venv -y 
 
 python3 -m venv .venv && . .venv/bin/activate && python3 -m pip install -r requirements.txt
 
