61bb834e3adf0204448d9bcc183d295bc18e5cc4
lucasrodrigges <rodriggeslucas@gmail.com>
2023-05-11 18:11:16 -0300

    fix main's path

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ sudo apt-get install python3 python3-pip python3.10-venv -y
 
 python3 -m venv .venv && . .venv/bin/activate && python3 -m pip install -r requirements.txt
 
-cd src && python3 main.py
+cd /src
+
+python3 main.py
