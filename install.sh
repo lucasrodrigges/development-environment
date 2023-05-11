08f34930ada7b8bac797b12344d122aa03920fda
lucasrodrigges <rodriggeslucas@gmail.com>
2023-05-11 18:09:22 -0300

    fix(install.sh): fiz main's path

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ sudo apt-get install python3 python3-pip python3.10-venv -y
 
 python3 -m venv .venv && . .venv/bin/activate && python3 -m pip install -r requirements.txt
 
-python3 ./src/main.py
+cd src && python3 ./src/main.py
